5f7c2c06b3bb51fa7b40078b93709d237a9c8201
IamHungDo <hung95do@yahoo.com>
2017-01-05 14:14:13 -0800

    Tip Calulator
    Code Path University Submission

---
+++ Tipster/Tipster/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Hung Do on 1/4/17.
+//  Copyright © 2017 Hung Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipController: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercent = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercent[tipController.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+}
+
